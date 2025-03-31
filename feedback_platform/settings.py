4cecd7d862ab63823dd04f89e30791a6013d5ab1
Onserio Ogeto <onserioogeto@gmail.com>
2025-03-31 13:59:01 +0300

    added files to try deployment on Render

--- feedback_platform/settings.py
+++ feedback_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i22h9+37eg0vbp6xy2we(=yuld=l^pozxwekl*&@v!2^^hk2lb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'feedback_platform.urls'
@@ -127,6 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
